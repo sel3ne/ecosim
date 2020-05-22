b5aa3a8ceab7c79a6fd1c392552c3f1f5d9870ce
Ella von Vintschger <ellavin@gmail.com>
2020-05-22 20:59:55 +0200

    fixed compiling problems

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,13 +1,13 @@
 #include "grid.h"
 
 sf::Vector2i worldCoordinateToGrid(sf::Vector2f worldCoordinate) {
-  int x = worldCoordinat.x / 10;
-  int y = worldCoordinat.y / 10;
+  int x = worldCoordinate.x / 10;
+  int y = worldCoordinate.y / 10;
   return sf::Vector2i(x, y);
 }
 
 sf::Vector2i worldCoordinateToGrid(sf::Vector2i worldCoordinate) {
-  int x = worldCoordinat.x / 10;
-  int y = worldCoordinat.y / 10;
+  int x = worldCoordinate.x / 10;
+  int y = worldCoordinate.y / 10;
   return sf::Vector2i(x, y);
 }
