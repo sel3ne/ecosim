83c247d096bc34f19c0772b51bf80c5a6547ddc1
Ella von Vintschger <ellavin@gmail.com>
2020-05-31 13:30:20 +0200

    fixed rounding for negative numbers

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,13 +1,15 @@
 #include "grid.h"
 
+#include <cmath>
+
 sf::Vector2i worldCoordinateToGrid(sf::Vector2f worldCoordinate) {
-  int x = worldCoordinate.x / kPixelsPerTile;
-  int y = worldCoordinate.y / kPixelsPerTile;
+  int x = std::floor(worldCoordinate.x / kPixelsPerTile);
+  int y = std::floor(worldCoordinate.y / kPixelsPerTile);
   return sf::Vector2i(x, y);
 }
 
 sf::Vector2i worldCoordinateToGrid(sf::Vector2i worldCoordinate) {
-  int x = worldCoordinate.x / kPixelsPerTile;
-  int y = worldCoordinate.y / kPixelsPerTile;
+  int x = std::floor(static_cast<float>(worldCoordinate.x) / kPixelsPerTile);
+  int y = std::floor(static_cast<float>(worldCoordinate.y) / kPixelsPerTile);
   return sf::Vector2i(x, y);
 }
