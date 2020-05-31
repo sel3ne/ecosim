5be38ab5b3914ac3a48d5517fe497d90ba925c2b
Conrad Burchert <conrad.burchert@googlemail.com>
2020-06-01 00:44:46 +0200

    Improve food indicator in the top bar.

--- src/settings.h
+++ src/settings.h
@@ -8,4 +8,8 @@ const float kFoodProductionFarm = 100 / 60;
 const float kSpeedHuman = 8;
 const float kMaxFoodPerFarm = 1.0f;
 
+const float kTopBarHeight = 30.f;
+const int kTopBarTextSize = 22;
+const int kFoodStatusX = 400;
+
 #endif  // define ECOSIM_SETTINGS_H
