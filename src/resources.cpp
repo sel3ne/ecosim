40006e081aebaeb20a1352b9d03d94282b0a74f3
Ella von Vintschger <ellavin@gmail.com>
2020-10-11 00:51:20 +0200

    fixed name of test_resource

--- src/resources.cpp
+++ src/resources.cpp
@@ -1,4 +1,6 @@
 #include "resources.h"
 
-const std::map<ResourceId, std::string> kResourceNames{{RESOURCE_FOOD, "Food"},
-                                                       {RESOURCE_GOLD, "Gold"}};
+const std::map<ResourceId, std::string> kResourceNames{
+    {RESOURCE_FOOD, "Food"},
+    {RESOURCE_GOLD, "Gold"},
+    {RESOURCE_TEST, "Test Resource"}};
